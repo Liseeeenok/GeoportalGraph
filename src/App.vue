a030bfe66ebf2d6ca62c278341c0f356bf82e004
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-08-17 16:08:08 +0800

    Создание vue приложения

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <graph-geoportal/>
+</template>
+
+<script>
+import GraphGeoportal from '@/components/GraphGeoportal.vue'
+export default {
+    components: {
+        GraphGeoportal,
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/GraphGeoportal.vue
@@ -0,0 +1,38 @@
+<template>
+    <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :height="heightGraph"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+export default {
+    components: {
+        Bar,
+    },
+    data() {
+        return {
+            heightGraph: 100,
+            chartData: {
+                labels: [ 'January', 'February', 'March' ],
+                datasets: [ 
+                    { 
+                        data: [4, 2, 1] 
+                    } 
+                ]
+            },
+            chartOptions: {
+                responsive: true,
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
