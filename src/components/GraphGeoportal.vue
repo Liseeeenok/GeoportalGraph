ebda5afd4f42184bc5cbd9c53900df936306e2f9
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-09-03 15:24:22 +0800

    Рефакторинг кода и возможность добавить график

--- src/components/GraphGeoportal.vue
+++ src/components/GraphGeoportal.vue
@@ -1,58 +1,68 @@
 <template>
-    <div class="row row-cols-1 row-cols-lg-2 align-items-center justify-content-between">
+    <div class="row row-cols-1 align-items-center justify-content-between">
         <Bar
-            :class="idEx === false ? 'col-lg-8' : 'col-lg-12'"
+            :class="idEx === false ? 'col-lg-9' : 'col-lg-12'"
             :chart-options="this.graph.chartOptions"
             :chart-data="this.graph.chartData"
             :height="heightGraph"
         />
-        <div v-if="idEx === false" class="container col-lg-4">
+        <div v-if="idEx === false" class="container col-lg-3">
             <div class="row row-cols-1 justify-content-center align-items-center">
                 <div class="mb-1">
                     Название графика:
                 </div>
-                <div class="row row-cols-1 justify-content-center col-10 mb-1">
+                <div class="row row-cols-1 justify-content-center col-12 mb-1">
                     <input placeholder="Введите название графика" v-model="this.selectedTitle">
                 </div>
-                <div class="mb-1">
-                    Цвет графика: rgb({{selectedColor.red}},{{selectedColor.green}},{{selectedColor.blue}})
+                <div class="row row-cols-2 mt-1">
+                    <div class="col pe-0 ps-0">Отображать с:</div>
+                        <select class="col-6" @change="sliceLabelsGraph()" v-model="selectedFirstDate">
+                            <option disabled value="">Выберите дату</option>
+                            <option v-for="dat in labelsGraph" :value="dat" :key="dat">{{dat}}</option>
+                        </select>
+                    <div class="col mt-1">по:</div>
+                        <select class="col-6 mt-1" @change="sliceLabelsGraph()" v-model="selectedEndDate">
+                            <option disabled value="">Выберите дату</option>
+                            <option v-for="dat in labelsGraph" :value="dat" :key="dat">{{dat}}</option>
+                        </select>
                 </div>
-                <div class="row row-cols-2 justify-content-center col-10">
+                <div class="row col-8 align-self-end mt-3">
+                    <button @click="addGraph()" class="p-2 btn btn-outline-primary">Добавить график</button>
+                </div>
+                <div class="row col-8 align-self-end mt-3">
+                    <button @click="generationId()" class="p-2 btn btn-outline-primary">Сохранить график</button>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div v-if="idEx === false" class="container row pe-0 ps-0 justify-content-center">
+            <div v-for="n in countGraph" :key="n-1" class="row justify-content-center align-items-center col-12 col-sm-6 col-md-4 col-lg-3 border rounded ms-3 mt-1 pe-1 ps-1">
+                <div class="mb-1 pt-2">
+                    Цвет графика №{{n}}:<br> rgb({{selectedColor[n-1].red}},{{selectedColor[n-1].green}},{{selectedColor[n-1].blue}})
+                </div>
+                <div class="row row-cols-2 justify-content-center col-12">
                     <div class="col-1 p-1 mb-1" style="background-color: red"></div>
-                    <input class="col-11 mb-1" type="range" min="0" max="255" step="1" v-model="this.selectedColor.red" :oninput="changeColor()"/>
+                    <input class="col-11 mb-1 pe-0 ps-0" type="range" min="0" max="255" step="1" v-model="this.selectedColor[n-1].red" :oninput="changeColor(n-1)"/>
                     <div class="col-1 p-1 mb-1" style="background-color: green"></div>
-                    <input class="col-11 mb-1" type="range" min="0" max="255" step="1" v-model="this.selectedColor.green" :oninput="changeColor()"/>
+                    <input class="col-11 mb-1 pe-0 ps-0" type="range" min="0" max="255" step="1" v-model="this.selectedColor[n-1].green" :oninput="changeColor(n-1)"/>
                     <div class="col-1 p-1 mb-1" style="background-color: blue"></div>
-                    <input class="col-11 mb-1" type="range" min="0" max="255" step="1" v-model="this.selectedColor.blue" :oninput="changeColor()"/>
+                    <input class="col-11 mb-1 pe-0 ps-0" type="range" min="0" max="255" step="1" v-model="this.selectedColor[n-1].blue" :oninput="changeColor(n-1)"/>
                 </div>
-                <select class="col-10 mt-3" @change="redrawGraph(); changeId()" v-model="selectedTer">
-                    <option disabled value="" selected="true">Выберите один из вариантов</option>
+                <select class="col-12 mt-1" @change="redrawGraph(n-1); changeId()" v-model="selectedTer[n-1]">
+                    <option disabled :value='{ "data":"" }'>Выберите территорию</option>
                     <option v-for="ter in arrTer" :key="ter.id" :value="ter">{{ter.name}}</option>
                 </select>
-                <div class="row col-10 justify-content-center pe-0 ps-0 mt-2 ">
-                    <div class="row col-10 justify-content-center"><span class="col-10">Вертикальные данные:</span></div>
-                    <div class="row col-7">
-                        <select @change="redrawGraph(); changeId()" v-model="selectedMean">
-                            <option disabled value="">Выберите один из вариантов</option>
+                <div class="row col-10 justify-content-center pe-0 ps-0 mt-1">
+                    <div class="row col-12 justify-content-center"><span class="col-12">Вертикальные данные:</span></div>
+                    <div class="row col-10 mt-1 pb-2">
+                        <select @change="redrawGraph(n-1); changeId()" v-model="selectedMean[n-1].data">
+                            <option disabled :value='""'>Выберите параметр</option>
                             <option value="t">Температура</option>
                             <option value="po">Давление</option>
                         </select>
                     </div>
-                </div>
-                <div class="row row-cols-2 mt-3">
-                    <div class="col">Отображать с:</div>
-                    <select class="col-6" @change="sliceLabelsGraph()" v-model="selectedFirstDate">
-                            <option disabled value="">Выберите один из вариантов</option>
-                            <option v-for="dat in labelsGraph" :value="dat" :key="dat">{{dat}}</option>
-                        </select>
-                    <div class="col mt-3">по:</div>
-                    <select class="col-6 mt-3" @change="sliceLabelsGraph()" v-model="selectedEndDate">
-                            <option disabled value="">Выберите один из вариантов</option>
-                            <option v-for="dat in labelsGraph" :value="dat" :key="dat">{{dat}}</option>
-                        </select>
-                </div>
-                <div class="row col-7 align-self-end mt-3">
-                    <button @click="generationId()" class="p-2 btn btn-outline-primary">Сохранить график</button>
                 </div>
             </div>
         </div>
@@ -77,6 +87,7 @@ export default {
     },
     data() {
         return {
+            countGraph: 1,
             selectedFirstDate: '', //Первая дата графика
             selectedEndDate: '', //Последняя дата графика
             selectedTitle: '', //Название графика
@@ -85,21 +96,25 @@ export default {
                 green: '110',
                 blue: '253'
             },
-            selectedColor: {
+            selectedColor: [{
                 red: '',
                 green: '',
                 blue: ''
-            }, //Выбранный цвет графика
+            }], //Выбранный цвет графика
             idPage: '', //id Графика
             idEx: undefined, //Существует ли id графика
-            selectedMean: 't', //Вертикальная шкала графика
-            selectedTer: '', //Выбранная территория
+            selectedMean: [{
+                data: ''
+            }], //Вертикальная шкала графика
+            selectedTer: [{
+                data: ''
+            }], //Выбранная территория
             arrTer: [], //Массив территорий
             iDisplayLength: 3061, //кол-во запрашиваемых строк
             baseURL: 'http://cris.icc.ru/dataset/list?f=1875&count_rows=true&iDisplayStart=0', //Базовая ссылка (разбить)
             heightGraph: 200, //Высота графика (в пикселях)
             labelsGraph: [], //массив дат для графика
-            dataGraph: [], //массив данных для графика
+            dataGraph: [[]], //массив данных для графика
             graph: { //Настройки графика
                 chartData: { //Данные графика
                     labels: [], //Горизонтальные подписи
@@ -131,9 +146,9 @@ export default {
         changeId() { //Сбрасывает id при изменении графика
             this.URLforSave = false;
         },
-        redrawGraph() { //Функция для перерисовки графика
+        redrawGraph(index) { //Функция для перерисовки графика
             let chartlabel = '';
-            const arrData = this.selectedTer.data; //Берём данные выбранной территории
+            const arrData = this.selectedTer[index].data; //Берём данные выбранной территории
             const chartDataLabel = []; //Массив для времени
             const chartDataDatasets = []; //Массив для данных
             arrData.sort((prev, next) => { //Сортируем массив по дате
@@ -142,14 +157,14 @@ export default {
             });
             arrData.map(Ter => { //Заполняем массивы данными
                 if ((Ter.w_date !== null)) { //Если существует дата
-                    if ((this.selectedMean === 't') && (Ter.t !== null)) { //Если выбран график по температуре
-                        chartlabel = this.selectedMean + ' ' + Ter.wmoid.name;
+                    if ((this.selectedMean[index].data === 't') && (Ter.t !== null)) { //Если выбран график по температуре
+                        chartlabel = this.selectedMean[index].data + ' ' + Ter.wmoid.name;
                         chartDataLabel.push(Ter.w_date);
                         chartDataDatasets.push(Ter.t);
                         return;
                     };
-                    if ((this.selectedMean === 'po') && (Ter.po !== null)) { //Если выбран график по давлению
-                        chartlabel = this.selectedMean + ' ' + Ter.wmoid.name;
+                    if ((this.selectedMean[index].data === 'po') && (Ter.po !== null)) { //Если выбран график по давлению
+                        chartlabel = this.selectedMean[index].data + ' ' + Ter.wmoid.name;
                         chartDataLabel.push(Ter.w_date);
                         chartDataDatasets.push(Ter.po);
                         return;
@@ -157,10 +172,10 @@ export default {
                     
                 };
             });
-            this.graph.chartData.datasets[0].label = chartlabel; //Присваеваем данные графику 
+            this.graph.chartData.datasets[index].label = chartlabel; //Присваеваем данные графику 
             this.labelsGraph = chartDataLabel;
             //this.graph.chartData.labels = chartDataLabel; Убрано, чтобы график не загружался сразу после выбора территории
-            this.dataGraph = chartDataDatasets;
+            this.dataGraph[index] = chartDataDatasets;
             //this.graph.chartData.datasets[0].data = chartDataDatasets;
             this.sliceLabelsGraph() //Обрезка данных по условию
         },
@@ -207,14 +222,14 @@ export default {
                 }
             } else {
                 await this.getDataAPI(); //Запрашиваем апи
-                this.selectedColor = this.standartColor; // Стандартный цвет для графика
+                this.selectedColor[0] = JSON.parse(JSON.stringify(this.standartColor)); // Стандартный цвет для графика
+
                 this.idEx = false;
-                this.redrawGraph();
             }
         },
         generationId() { //Создаёт id графика
             const saveObj = {}; //Объект для отправки
-            const saveId = `${this.selectedTer.id}${this.selectedMean}${Date.now()}`; //id (id местности)(данные)(дата)
+            const saveId = `${this.selectedTer[0].id}${this.selectedMean[0].data}${Date.now()}`; //id (id местности)(данные)(дата)
             const saveDataGraph = this.graph; //Данные графика
             saveObj.id = saveId;
             saveObj.dataGraph = saveDataGraph;
@@ -222,8 +237,8 @@ export default {
 
             console.log(JSON.stringify(saveObj)); //Отправка этого объекта в БД
         },
-        changeColor() { //Изменяет цвет графика
-            this.graph.chartData.datasets[0].backgroundColor = `rgb(${this.selectedColor.red},${this.selectedColor.green},${this.selectedColor.blue}`;
+        changeColor(index) { //Изменяет цвет графика
+            this.graph.chartData.datasets[index].backgroundColor = `rgb(${this.selectedColor[index].red},${this.selectedColor[index].green},${this.selectedColor[index].blue}`;
         },
         changeLabel() { //Изменяет название графика
             this.graph.chartOptions.plugins.title.text = this.selectedTitle;
@@ -233,8 +248,16 @@ export default {
                 const FirstIndex = this.labelsGraph.indexOf(this.selectedFirstDate);
                 const EndIndex = this.labelsGraph.indexOf(this.selectedEndDate);
                 this.graph.chartData.labels = this.labelsGraph.slice(FirstIndex, EndIndex+1);
-                this.graph.chartData.datasets[0].data = this.dataGraph.slice(FirstIndex, EndIndex+1);
+                this.dataGraph.forEach((el, index) => this.graph.chartData.datasets[index].data = el.slice(FirstIndex, EndIndex+1));
             }
+        },
+        addGraph() {
+            this.selectedColor[this.countGraph] = JSON.parse(JSON.stringify(this.standartColor));
+            this.selectedMean.push({data: ""});
+            this.selectedTer.push({data: ""});
+            this.dataGraph[this.countGraph] = [];
+            this.graph.chartData.datasets.push({label: '', data: [], backgroundColor: ''});
+            this.countGraph++;
         }
     },
     created() {
